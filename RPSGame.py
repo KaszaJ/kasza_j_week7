826792bd8dccca7412c079930141ee982e7aee1b
KaszaJ <j_kasza2@fanshaweonline.ca>
2018-10-31 22:36:31 -0400

    Final updates on RPS and README

--- RPSGame.py
+++ RPSGame.py
@@ -1,19 +1,25 @@
 from random import randint
 
-# available weapons => store this in an array
+#available weapons => store this in an array
 choices = ["Rock", "Paper", "Scissors"]
 player = False
 
-# make the computer pick one item at random
+#make the computer pick one item at random
 computer = choices[randint(0,2)]
 
 #shows the computer choice in the terminal window
-print("Computer chooses: ", computer)
+#(I commented the following line out because it ruins the point of the game! Hope that's okay!)
+#print("Computer chooses: ", computer)
+
+#3 lives for user and computer
+userLives = 3
+compLives = 3
 
 while player == False:
-    print("Choose Your Weapon\n")
-    player = input("Rock,Paper or Scissors?\n")
-    print("Player chooses:", player)
+    print("Choose Your Weapon")
+    print("********************************")
+    player = input("Rock, Paper or Scissors?\n")
+    print("\nPlayer chooses:", player, "\n")
 
    #check for equality
     if (player == computer):
@@ -21,30 +27,69 @@ while player == False:
 
     elif player == "Rock":
         if computer == "Paper":
-        # computer won
+        #computer won
             print("You lose!", computer, "covers", player)
+            #take away 1 life from user
+            userLives = userLives - 1
         else:
-            print("You win", player, "smashes", computer)
+            print("You win!", player, "smashes", computer)
+            #take away 1 life from computer
+            compLives = compLives - 1
 
 
     elif player == "Paper":
         if computer == "Scissors":
             print("You lose!", computer, "cuts", player)
+            userLives = userLives - 1
         else:
-            print("You win", player, "covers", computer)
+            print("You win!", player, "covers", computer)
+            compLives = compLives - 1
 
     elif player == "Scissors":
         if computer == "Rock":
-            print("You lose", computer, "smashes", player)
+            print("You lose!", computer, "smashes", player)
+            userLives = userLives - 1
         else:
             print("You win!", player, "cuts", computer)
+            compLives = compLives - 1
 
     elif player == "Quit":
         exit()
 
 
     else:
-        print("Not a valid option. Check again. and check your spelling\n")
+        print("Not a valid option. Please verify your input and check your spelling\n")
+
+    #show user players' lives
+    print("\nYour Lives:", userLives)
+    print("Compuer Lives:", compLives, "\n")
+
+    #tell user (if) they lost
+    if userLives == 0:
+        print("This game is complete.\nThe Computer won, better luck next time!\n")
+        print("********************************")
+        playAgain = input("Would you like to play again? Type 'Yes' or 'No':\n")
+        #ask user to play again
+        if(playAgain == "Yes"):
+            userLives = 3
+            compLives = 3
+        elif playAgain == "No":
+            exit()
+        else:
+            print("Not a valid option. Please verify your input and check your spelling\n")
+
+    #tell user (if) the computer lost
+    elif compLives == 0:
+        print("This game is complete.\nCongratulations, you won!\n")
+        print("********************************")
+        playAgain = input("Would you like to play again? Type 'Yes' or 'No':\n")
+        if(playAgain == "Yes"):
+            userLives = 3
+            compLives = 3
+        elif playAgain == "No":
+            exit()
+        else:
+            print("Not a valid option. Please verify your input and check your spelling\n")
 
     player = False
     computer = choices[randint(0, 2)]
